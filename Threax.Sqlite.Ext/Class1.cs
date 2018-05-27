1547dbce6a4054014f4e10c9b8685c47842199c9
Threax <andrew@threax.com>
2018-05-26 21:12:55 -0400

    Initial revision.

--- Threax.Sqlite.Ext/Class1.cs
+++
@@ -1,8 +0,0 @@
-﻿using System;
-
-namespace Threax.Sqlite.Ext
-{
-    public class Class1
-    {
-    }
-}
---
+++ Threax.Sqlite.Ext/SqliteFileExtensions.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.IO;
+
+namespace Threax.Sqlite.Ext
+{
+    public class SqliteFileExtensions
+    {
+        private static String DataSourceStart = "Data Source=";
+        private static String DataSourceEnd = ";";
+        private static char DataSourceEndChar = ';';
+
+        /// <summary>
+        /// This function will try to create a file from connection strings in the format:
+        /// Data Source=Path;
+        /// </summary>
+        /// <param name="connectionString"></param>
+        public static void TryCreateFile(string connectionString)
+        {
+            if (connectionString.StartsWith(DataSourceStart) && connectionString.EndsWith(DataSourceEnd))
+            {
+                var file = connectionString.Substring(DataSourceStart.Length);
+                file = file.TrimEnd(DataSourceEndChar);
+                file = Path.GetFullPath(file);
+                if (!File.Exists(file))
+                {
+                    var dir = Path.GetDirectoryName(file);
+                    if (!Directory.Exists(dir))
+                    {
+                        Directory.CreateDirectory(dir);
+                    }
+                    File.Create(file);
+                }
+            }
+        }
+    }
+}
