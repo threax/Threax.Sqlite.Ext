8d3a7a2b3325d0ac1ad96364e70ca9dc2585a382
Andrew Piper <andrew@threax.com>
2022-03-13 13:16:11 -0400

    Bump to full 3.0.0.

---
+++ Push.ps1
@@ -0,0 +1,8 @@
+git push
+git push --tags
+git push https://github.com/threax/Threax.Sqlite.Ext.git
+git push https://github.com/threax/Threax.Sqlite.Ext.git --tags
+git push https://gitlab.com/threax-software/Threax.Sqlite.Ext.git
+git push https://gitlab.com/threax-software/Threax.Sqlite.Ext.git --tags
+git push https://threax@dev.azure.com/threax/ThreaxSoftware/_git/Threax.Sqlite.Ext
+git push https://threax@dev.azure.com/threax/ThreaxSoftware/_git/Threax.Sqlite.Ext --tags
